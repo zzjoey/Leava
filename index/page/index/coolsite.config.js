a23c3d270f1c35703ebed8e57278bbd8ff9f8f6e
Joey <2571078340@qq.com>
2018-02-06 22:20:49 +0800

    index update
    home page update

---
+++ index/page/index/coolsite.config.js
@@ -0,0 +1,34 @@
+
+var actions = [];
+
+var animations = [];
+
+var timelines = [
+  {
+    "iType": 0, 
+    "isNew": true, 
+    "animations": [], 
+    "element_id": "body_aff43f0acf48aab5", 
+    "data": {
+      "type": 0, 
+      "t": {
+        "rv": 0, 
+        "rp": 0, 
+        "wa": 0, 
+        "de": 0, 
+        "st": 1, 
+        "du": 1, 
+        "es": 0
+      }, 
+      "d": {}
+    }, 
+    "id": "M_13afa1419f39a2bb"
+  }
+];
+
+getApp().coolsite360.DATA[__wxRoute] = {
+   animations:animations,
+   actions:actions,
+   timelines:timelines
+};
+
