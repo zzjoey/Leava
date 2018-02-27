c50727384e0df7d2479cde430cc082fe26ab6c3d
Joey <2571078340@qq.com>
2018-02-27 18:58:30 +0800

    update 学生端和管理端
    update 学生端和管理端的静态页面

---
+++ 辅导员端/page/four/coolsite.config.js
@@ -0,0 +1,34 @@
+
+var actions = [];
+
+var animations = [];
+
+var timelines = [
+  {
+    "iType": 0, 
+    "isNew": true, 
+    "animations": [], 
+    "element_id": "body_0f48aaad65786da0", 
+    "data": {
+      "type": 0, 
+      "t": {
+        "rv": 0, 
+        "rp": 0, 
+        "wa": 0, 
+        "de": 0, 
+        "st": 1, 
+        "du": 1, 
+        "es": 0
+      }, 
+      "d": {}
+    }, 
+    "id": "M_8143a308a5fd2130"
+  }
+];
+
+getApp().coolsite360.DATA[__wxRoute] = {
+   animations:animations,
+   actions:actions,
+   timelines:timelines
+};
+
