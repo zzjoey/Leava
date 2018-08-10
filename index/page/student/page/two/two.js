7b3a0d7b116e46db1715bcca942737c667ae6d01
Fuhaixu <31333610+Fuhaixu@users.noreply.github.com>
2018-08-10 15:16:19 +0800

    登录，页面跳转功能 (#1)
    * Add files via upload
    
    * delete

--- index/page/student/page/two/two.js
+++ index/page/student/page/two/two.js
@@ -33,6 +33,25 @@ Page({
     modalHidden:true,   
     txt:'对话框',   //对话框测试标题
     leaveinfo:'',
+    startTime:'',
+    tempFilePaths:'../../../../resources/z_add.png'
+  },
+
+  pic: function (options) {
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths[0]
+      }
+    })
+  },
+  fail: function (res) {
+    console.log(res.errMsg)
+  },
+
     startTime:''
   },
   bindTextAreaBlur: function (e) {
