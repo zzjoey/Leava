97f2f5243b05a9f70b07a7feb3e4ecdfac1c5288
Joey <zzjoeyyy@foxmail.com>
2020-06-30 17:52:30 +0800

    update backend port
    backend port 8080 -> 80

--- backend/RESTful_backend.py
+++ backend/RESTful_backend.py
@@ -649,4 +649,4 @@ def change_passwd(userId, userPwd):
 
 if __name__ == '__main__':
     # db = None
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
